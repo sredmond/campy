008d2311381f203d1acf557b000b08c3f5d96cb2
Sam Redmond <sredmond@stanford.edu>
2017-12-19 04:38:16 -0800

    Imports collections package absolutely.

--- spgl/util/point.py
+++ spgl/util/point.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3 -tt
-"""
-File: point.h
--------------
-Exports a type representing 2- and 3-dimensional points.
+"""Exports a type representing 2- and 3-dimensional points.
+
 These points inherit from tuples, so you can do anything
 you could otherwise do with tuples (notably, tuple unpacking)
 
-Usage:
+Usage::
 
     origin = Point()
     pt = Point(4, 1)
@@ -16,9 +14,16 @@ Usage:
     origin3 = Point3()
     pt = Point3(3, 4, 5)
     x, y, z = pt
-
 """
-from collections import namedtuple as _nt
+import collections as _collections
 
-Point = _nt('Point', ['x', 'y'])
-Point3 = _nt('Point', ['x', 'y', 'z'])
+Point = _collections.namedtuple('Point', ['x', 'y'])
+Point3 = _collections.namedtuple('Point3', ['x', 'y', 'z'])
+
+# Override property documentation
+# TODO(sredmond): For some reason, the property's __doc__ attribute is readonly.
+# Point.x.__doc__ = "The x-coordinate of a 2D point."
+# Point.y.__doc__ = "The y-coordinate of a 2D point."
+# Point3.x.__doc__ = "The x-coordinate of a 3D point."
+# Point3.y.__doc__ = "The y-coordinate of a 3D point."
+# Point3.z.__doc__ = "The z-coordinate of a 3D point."
