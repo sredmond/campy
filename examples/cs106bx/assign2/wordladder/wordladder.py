d57885ba42a6ae02d421e8baf005355e4d3fe8b9
Sam Redmond <sredmond@stanford.edu>
2017-12-19 04:19:58 -0800

    Adds example program for CS106BX A2: NGrams/WordLadder.

---
+++ examples/cs106bx/assign2/wordladder/wordladder.py
@@ -0,0 +1,21 @@
+
+"""Example implementatino of CS106B Assignment 2: WordLadder.
+
+"""
+from spgl.datastructures.lexion import Lexicon
+from spgl.util.simpio import get_line
+
+
+def greet():
+    print("Welcome to CS 106B/X Word Ladder!")
+    print("Please give me two English words, and I will convert the")
+    print("first into the second by modifying one letter at a time.")
+    print()
+
+def load_dictionary():
+    # TODO(sredmond): Handle bad dictionary name.
+    return Lexicon(get_line("Dictionary file name:"))
+
+if __name__ == '__main__':
+    greet()
+    # TODO(sredmond): Finish wordladder!
