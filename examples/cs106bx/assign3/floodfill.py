c647a32fda8361a14cf40f9b55de626aa7e3e3ba
Sam Redmond <sredmond@stanford.edu>
2017-12-19 04:16:51 -0800

    Adds example program to CS106BX A3: Recursion.

---
+++ examples/cs106bx/assign3/floodfill.py
@@ -0,0 +1,1 @@
+# TODO(sredmond): Implement floodfill.
---
+++ examples/cs106bx/assign3/sierpinski.py
@@ -0,0 +1,22 @@
+import math
+
+from spgl.graphics.gwindow import GWindow
+from spgl.graphics.gobjects import *
+
+def draw_sierpinski_triangle(window, x, y, size, order):
+    height = size * math.sqrt(3) / 2
+    if order == 0:
+        window.add(GLine(x, y, x+size, y))  # Horizontal segment.
+        window.add(GLine(x, y, x+size/2, y+height))  # Left inclined segment.
+        window.add(GLine(x+size/2, y+height, x+size, y))  # Right inclined segment.
+    else:
+        draw_sierpinski_triangle(window, x, y, size/2, order-1)
+        draw_sierpinski_triangle(window, x+size/2, y, size/2, order-1)
+        draw_sierpinski_triangle(window, x+size/4, y+height/2, size/2, order-1)
+
+if __name__ == '__main__':
+    size = 1080
+    order = 5
+
+    gw = GWindow(width=size, height=size)
+    draw_sierpinski_triangle(gw, 0, 0, size, order)
