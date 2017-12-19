05b68733e8e9c902e39bd03fe722a551a1553e20
Sam Redmond <sredmond@stanford.edu>
2017-12-19 04:26:11 -0800

    Moves random debugging scripts to debug/ top-level folder.

--- debug/run_text_commands/simple.py
+++ debug/run_text_commands/simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3 -tt
 """
-File: simple.py
----------------
 Tests the most basic function of the platform module, namely `putPipe`.
 """
 if __name__ == '__main__':
@@ -11,7 +9,7 @@ if __name__ == '__main__':
         choice = input('(P)ut or (G)et? ')
         if choice.lower().startswith('p'):
             cmd = input("putPipe: ")
-            _platform.Platform().putPipe(cmd)
+            _platform.Platform().put_pipe(cmd)
         if choice.lower().startswith('g'):
-            result = _platform.Platform().getResult()
+            result = _platform.Platform().get_result()
             print(result)
