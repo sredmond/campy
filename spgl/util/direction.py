cd865e89ff740adb6f262fbe1dd50dae0a0f720a
Sam Redmond <sredmond@stanford.edu>
2017-12-19 04:37:15 -0800

    Adds Sphinx-like documentation to Directions.

--- spgl/util/direction.py
+++ spgl/util/direction.py
@@ -1,44 +1,43 @@
-#!/usr/env/bin python3 -tt
-"""
-File: direction.py
-------------------
-Exports an enumerated type `Direction` whose elements are the four cardinal
-directions: NORTH, SOUTH, EAST, and WEST.
+"""An enumerated type :class:`Direction` whose elements are the four cardinal directions:
+``NORTH``, ``SOUTH``, ``EAST``, and ``WEST``.
 
-A direction knows what direction is to the left, the right, or opposite itself
-using the methods `left()`, `right()`, and `opposite()`
+A :class:`Direction` knows what :class:`Direction` is to the left, the right, or opposite itself
+using the methods :func:`left`, :func:`right`, and :func:`opposite`.
 
-Usage:
+Usage::
 
     direction = Direction.NORTH
     direction.left()      # => Direction.WEST
     direction.right()     # => Direction.EAST
     direction.opposite()  # => Direction.SOUTH
 """
-from enum import Enum, unique
+import enum
 
-@unique
-class Direction(Enum):
-    """This enumerated type represents compass directions."""
+@enum.unique
+class Direction(enum.Enum):
+    """Represent the four compass directions."""
     NORTH = 0
     EAST = 1
     SOUTH = 2
     WEST = 3
 
     def left(self):
-        """Returns the direction that is to the left of self."""
+        """Return the direction to the left."""
         return self.__class__((self.value + 3) % 4)
 
     def right(self):
-        """Returns the direction that is to the right of self."""
+        """Return the direction to the right."""
         return self.__class__((self.value + 1) % 4)
 
     def opposite(self):
-        """Returns the direction that is opposite to self."""
+        """Return the opposite direction."""
         return self.__class__((self.value + 2) % 4)
 
     def __repr__(self):
+        # Only show Direction.NAME, instead of the default '<Direction.NAME: value>`.
         return "{}.{}".format(self.__class__.__name__, self.name)
+
+__all__ = ['Direction']
 
 if __name__ == '__main__':
     direction = Direction.NORTH
