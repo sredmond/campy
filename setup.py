325efbdc03308d93c51743c593fb8502f17e658b
Sam Redmond <sredmond@stanford.edu>
2018-10-27 12:20:28 -0700

    Updates distributed package name from spl to campy.

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ from setuptools import setup, find_packages
 #     long_description = f.read()
 
 setup(
-    name='spl',
+    name='campy',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1.dev1',
+    version='0.0.1.dev2',
 
     description='Python Client for Stanford Libraries, including Stanford\'s Portable Graphics Library',
     # long_description=long_description,
