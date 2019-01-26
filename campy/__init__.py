1a6eef70ed84a8ee6c34508d4e18c7a3ec7f3973
Sam Redmond <sredmond@stanford.edu>
2019-01-26 12:25:36 -0800

    Adds default null handler to entire campy library.

--- campy/__init__.py
+++ campy/__init__.py
@@ -1,8 +1,13 @@
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 __author__ = "Sam Redmond"
-__copyright__ = "Copyright 2016"
+__copyright__ = "Copyright 2016-2019"
 __credits__ = ["Sam Redmond", "Alex Valderrama", "Marty Stepp", "Eric Roberts"]
 __license__ = "GPL"
 __version__ = "0.1.0"
 __maintainer__ = "Sam Redmond"
 __email__ = "sredmond@stanford.edu"
 __status__ = "Prototype"
+
