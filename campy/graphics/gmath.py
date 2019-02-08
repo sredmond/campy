2e7bbfeb8f2a6f36767e4cad11ea3d09f62adb47
Sam Redmond <sredmond@stanford.edu>
2019-02-07 22:01:57 -0800

    Preparatory pass over gmath, adding more informative header comments.

--- campy/graphics/gmath.py
+++ campy/graphics/gmath.py
@@ -1,8 +1,17 @@
-'''
-This file exports several functions for working with graphical gemoetry along with
-the mathematical constants PI and E
-'''
-import math as _math
+"""Utility functions for working with 2D geometry degrees, vectors, and constants.
+
+Most Python libraries (such as the ``math`` module) expect angles to be specified
+in radians. While this is a good convention, new students may have trouble using
+radians, so this module provides degree-accepting variants of the standard trig
+functions, as well as a utility to convert between radians and degrees.
+
+Students often represent 2D vectors by their components, so we also provide
+conversion to vector magnitude and vector angle.
+
+The mathematical constants ``PI`` and ``E`` are also exported so that students
+don't have to import a separate math module for those constants.
+"""
+import math
 
 from campy.system.error import error
 
@@ -18,97 +27,104 @@ The mathematical constant e, which is the base of the natural logarithm
 '''
 
 def to_degrees(radians):
-	'''
-	Converts an angle from radians to degrees.
+    '''
+    Converts an angle from radians to degrees.
 
-	@type radians: float
-	@param radians: radians
-	@rtype: float
-	@return: degrees
-	'''
-	return _math.degrees(radians)
+    @type radians: float
+    @param radians: radians
+    @rtype: float
+    @return: degrees
+    '''
+    return _math.degrees(radians)
 
 def to_radians(degrees):
-	'''
-	Converts an angle from degrees to radians.
+    '''
+    Converts an angle from degrees to radians.
 
-	@type degrees: float
-	@param degrees: degrees
-	@rtype: float
-	@return: radians
-	'''
-	return _math.radians(degrees)
+    @type degrees: float
+    @param degrees: degrees
+    @rtype: float
+    @return: radians
+    '''
+    return _math.radians(degrees)
 
 def sin_degrees(angle):
-	'''
-	Returns the trigonometric sine of angle, which is expressed in degrees
+    '''
+    Returns the trigonometric sine of angle, which is expressed in degrees
 
-	@type angle: float
-	@param angle: degrees
-	@rtype: float
-	'''
-	return _math.sin(to_radians(angle))
+    @type angle: float
+    @param angle: degrees
+    @rtype: float
+    '''
+    return _math.sin(to_radians(angle))
 
 def cos_degrees(angle):
-	'''
-	Returns the trigonometric cosine of angle, which is expressed in degrees
+    '''
+    Returns the trigonometric cosine of angle, which is expressed in degrees
 
-	@type angle: float
-	@param angle: degrees
-	@rtype: float
-	'''
-	return _math.cos(to_radians(angle))
+    @type angle: float
+    @param angle: degrees
+    @rtype: float
+    '''
+    return _math.cos(to_radians(angle))
 
 def tan_degrees(angle):
-	'''
-	Returns the trigonometric tangent of angle, which is expressed in degrees
+    '''
+    Returns the trigonometric tangent of angle, which is expressed in degrees
 
-	@type angle: float
-	@param angle: degrees
-	@rtype: float
-	'''
-	return _math.tan(to_radians(angle))
+    @type angle: float
+    @param angle: degrees
+    @rtype: float
+    '''
+    return _math.tan(to_radians(angle))
 
 def vector_distance(x, y):
-	'''
-	Computes the distance between the origin and the specified point.
+    '''
+    Computes the distance between the origin and the specified point.
 
-	@type pt: GPoint
-	@param pt: GPoint to compute distance to, will override x and y parameters
-	@type x: float
-	@param x: x value of point
-	@type y: float
-	@param y: y value of point
-	@rtype: float
-	'''
-	return _math.sqrt(x * x + y * y)
+    @type pt: GPoint
+    @param pt: GPoint to compute distance to, will override x and y parameters
+    @type x: float
+    @param x: x value of point
+    @type y: float
+    @param y: y value of point
+    @rtype: float
+    '''
+    return _math.sqrt(x * x + y * y)
 
 def vector_angle(x, y):
-	'''
-	Returns the angle in degrees from the origin to the specified point. This
-	functoin takes account of the fact that the graphics coordinate system is flipped
-	in the y direction from the traditional Cartesian plane.
+    '''
+    Returns the angle in degrees from the origin to the specified point. This
+    functoin takes account of the fact that the graphics coordinate system is flipped
+    in the y direction from the traditional Cartesian plane.
 
-	@type pt: GPoint
-	@param pt: GPoint to angle to, will override x and y parameters
-	@type x: float
-	@param x: x value of point
-	@type y: float
-	@param y: y value of point
-	@rtype: float
-	@return: degrees
-	'''
-	if x == 0 and y == 0:
-		return 0
-	return to_degrees(_math.atan2(-y, x))
+    @type pt: GPoint
+    @param pt: GPoint to angle to, will override x and y parameters
+    @type x: float
+    @param x: x value of point
+    @type y: float
+    @param y: y value of point
+    @rtype: float
+    @return: degrees
+    '''
+    if x == 0 and y == 0:
+        return 0
+    return to_degrees(_math.atan2(-y, x))
 
 def count_digits(n, base):
-	if base <= 0:
-		error("countDigits: base must be 1 or greater")
-	n = abs(n)
-	digits = 0
-	while n > 0:
-		digits += 1
-		n //= base
-	return digits
+    if base <= 0:
+        error("countDigits: base must be 1 or greater")
+    n = abs(n)
+    digits = 0
+    while n > 0:
+        digits += 1
+        n //= base
+    return digits
 
+__all__ = [
+    'PI', 'E',
+    'to_degrees', 'to_radians',
+    'sin_degrees', 'cos_degrees', 'tan_degrees',
+    'vector_magnitude', 'vector_angle',
+    'count_digits'
+]
