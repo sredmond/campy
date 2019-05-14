81657a46f1f293af28f0aba8288613d6c24df005
Sam Redmond <sredmond@stanford.edu>
2019-05-14 00:37:28 -0700

    Defines top-level package initialization and modularizes versioning.

--- campy/__init__.py
+++ campy/__init__.py
@@ -1,13 +1,44 @@
+"""
+CAMPY Graphics Library
+~~~~~~~~~~~~~~~~~~~~~~
+
+The campy library makes Stanford's graphical ACM libraries available in Python.
+
+# TODO(sredmond): Add basic usage examples.
+
+Full documentation is available at <https://campy.sredmond.io/>
+
+:copyright: (c) 2016-2019 by Sam Redmond.
+:license: MIT License. See LICENSE for more details.
+"""
+import warnings
+
+# TODO(sredmond): Check compatibility of dependency versions.
+
+# TODO(sredmond): Check backend availability.
+try:
+    import _tkinter
+except ImportError as err:
+    warnings.warn('Unable to find _tkinter library. If using the Tk backend, you will need to install a version of Python with Tk support.')
+
+try:
+    import tkinter
+except ImportError as err:
+    warnings.warn('Unable to find tkinter library. If using the Tk backend, you will need to install a version of Python with Tk support.')
+
+# Import package metadata.
+from .__version__ import (
+    __title__, __description__, __url__, __license__,
+    __version__, __build__, __status__, __author__,
+    __maintainer__, __email__, __copyright__, __credits__,
+    __snake__
+)
+
+# OPTIONAL(sredmond): Import subpackages to the top-level namespace.
+
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Sam Redmond"
-__copyright__ = "Copyright 2016-2019"
-__credits__ = ["Sam Redmond", "Alex Valderrama", "Marty Stepp", "Eric Roberts"]
-__license__ = "GPL"
-__version__ = "0.1.0"
-__maintainer__ = "Sam Redmond"
-__email__ = "sredmond@stanford.edu"
-__status__ = "Prototype"
-
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.info("Welcome to the campy libraries.")
+logger.info("If you have any questions, reach out to {me} at {email}".format(me=__maintainer__, email=__email__))
---
+++ campy/__version__.py
@@ -0,0 +1,16 @@
+__title__ = 'campy'
+__description__ = 'ACM Graphical Libraries in Python'
+__url__ = 'https://campy.sredmond.io/'
+__license__ = 'MIT'
+
+__version__ = '0.0.1.dev3'
+__build__ = 0x000001
+__status__ = 'Prototype'
+
+__author__ = 'Sam Redmond'
+__maintainer__ = 'Sam Redmond'
+__email__ = 'sredmond@stanford.edu'
+__copyright__ = '(c) 2016-2019 by Sam Redmond'
+__credits__ = ["Sam Redmond", "Alex Valderrama", "Marty Stepp", "Eric Roberts"]
+
+__snake__ = '\u2728 \U0001F40D \u2728'
