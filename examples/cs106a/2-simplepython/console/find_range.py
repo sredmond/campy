6ed9cdabcd49c2a6346e604d959fc8efdc6b7f1b
Sam Redmond <sredmond@stanford.edu>
2019-05-14 01:16:51 -0700

    Cleans up implementation and comments for console examples.

--- examples/cs106a/2-simplepython/console/find_range.py
+++ examples/cs106a/2-simplepython/console/find_range.py
@@ -1,4 +1,24 @@
+#!/usr/bin/env python
+"""CS106A Assignment 2 Example: Find Range
+
+Prompt a user repeatedly for integers until a sentinel is entered, then report
+the smallest and largest values entered.
+
+It is an error for the user to immediately enter the sentinel value without
+first entering at least one value.
+
+Sample Usage::
+$ python find_range.py
+This program finds the largest and smallest numbers.
+? 5
+? -1
+? 7
+? 0
+Smallest: -1
+Largest: 7
+"""
 from campy.util.simpio import get_integer
+
 
 # Value representing positive infinity.
 POSITIVE_INFINITY = float('inf')
@@ -6,28 +26,33 @@ POSITIVE_INFINITY = float('inf')
 # Value representing negative infinity.
 NEGATIVE_INFINITY = float('-inf')
 
-#
+# Input that terminates the program.
 SENTINEL = 0
+
 
 def main():
     print("This program finds the largest and smallest numbers.")
-
     smallest, largest = POSITIVE_INFINITY, NEGATIVE_INFINITY
+
+    # Loop until the user enters the SENTINEL value.
     while True:
-        n = get_integer('?:')
+        n = get_integer('?')
         if n == SENTINEL:
             break
+
+        # Possibly update the running lowest or highest values.
         if n < smallest:
             smallest = n
         if n > largest:
             largest = n
 
+    # The smallest and largest values will be set to their defaults values only
+    # if the user has entered no non-sentinel values.
     if smallest == POSITIVE_INFINITY or largest == NEGATIVE_INFINITY:
         print('You must enter at least one value before the sentinel!')
     else:
-        print('smallest:', smallest)
-        print('largest:', largest)
-
+        print('Smallest:', smallest)
+        print('Largest:', largest)
 
 
 if __name__ == '__main__':
--- examples/cs106a/2-simplepython/console/hailstone.py
+++ examples/cs106a/2-simplepython/console/hailstone.py
@@ -1,21 +1,77 @@
-from campy.util.simpio import get_integer
+#!/usr/bin/env python
+"""CS106A Assignment 2 Example: Hailstone
+
+Print the hailstone sequence for a user-supplied positive integer.
+
+The problem can be expressed as follows:
+
+- Pick some positive integer `n`.
+- If `n` is even, divide it by two.
+- If `n` is odd, multiply it by three and add one.
+- Continue this process until `n` is equal to one.
+
+As an example, consider starting with the number 15:
+
+    15 is odd, so I make 3n + 1: 46
+    46 is even so I take half: 23
+    23 is odd, so I make 3n + 1: 70
+    70 is even so I take half: 35
+    35 is odd, so I make 3n + 1: 106
+    106 is even so I take half: 53
+    53 is odd, so I make 3n + 1: 160
+    160 is even so I take half: 80
+    80 is even so I take half: 40
+    40 is even so I take half: 20
+    20 is even so I take half: 10
+    10 is even so I take half: 5
+    5 is odd, so I make 3n + 1: 16
+    16 is even so I take half: 8
+    8 is even so I take half: 4
+    4 is even so I take half: 2
+    2 is even so I take half: 1
+    The process took 17 step(s) to reach 1
+
+The numbers go up and down, but eventually - at least for all numbers that have
+ever been tried — ends at 1. In some respects, this process is reminiscent of
+the formation of hailstones, which get carried upward by the winds over and over
+again before they finally descend to the ground. This is where the name
+"hailstone sequence" comes from, although it goes by many other names as well.
+
+The hailstone sequence comes from Chapter XII of Douglas Hofstadter's
+Pulitzer-prize-winning book: "Godel, Escher, Bach," which contains many
+interesting mathematical puzzles, many of which can be expressed in the form of
+computer programs.
+
+No one has yet been able to prove that this process always stops. The number of
+steps in the process can certainly get very large. How many steps, for example,
+does the program take when n is 27?
+"""
+from campy.util.simpio import get_positive_integer
+
 
 def hailstone(n):
     steps = 0
+    # Until n is equal to one:
     while n != 1:
+        # If n is even, divide it by two.
         if n % 2 == 0:
             m = n // 2
             print('{} is even so I take half: {}'.format(n, m))
+        # If n is odd, multiply it by three and add one.
         else:
             m = 3 * n + 1
             print('{} is odd, so I make 3n + 1: {}'.format(n, m))
 
+        # Move to the next step in the hailstone sequence.
         n = m
         steps += 1
     return steps
 
 
 if __name__ == '__main__':
-    n = get_integer('Enter a number: ')
+    # Get a positive integer from the user.
+    n = get_positive_integer('Enter a number: ')
+
+    # Compute the hailstone sequence starting at that number.
     steps = hailstone(n)
     print('The process took {} step(s) to reach 1'.format(steps))
