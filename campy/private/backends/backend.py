8f79ed87dce2ba8265c7c1e7f238945ef93ae484
Sam Redmond <sredmond@stanford.edu>
2019-06-06 19:45:37 -0700

    Cleans up backend selection and graphics/console/audio backend interface.

--- campy/private/backends/backend.py
+++ campy/private/backends/backend.py
@@ -1,5 +1,66 @@
-from campy.private.backends.jbe.backend_jbe import JavaBackend
-from campy.private.backends.tk.backend_tk import TkBackend
+"""Load a campy graphical backend.
 
-# backend = TkBackend()
-backend = JavaBackend()
+The default graphical backend is Tk, but others are available in varying
+degrees of support.
+
+The available backends are:
+
+- Tk (default): Cross-platform graphics and widget library.
+- JBE: Text-based communication to a Java backend: `acm.jar`. Requires Java.
+
+This structure was modelled off of matplotlib's - for more information, see:
+
+https://matplotlib.org/faq/usage_faq.html#what-is-a-backend
+
+There are two ways to configure your backend. If there's a conflict, the last
+one in the list will be used.
+
+1. Setting the `CAMPY_BACKEND` environmental variable, either for your current
+shell or for a single script::
+
+    $ export CAMPY_BACKEND="Tk"
+    $ python test.py
+
+    $ CAMPY_BACKEND="tk" python test.py
+
+2. If your script depends on a specific backend you can call `use()`::
+
+    $ import campy
+    $ campy.use('tk')
+
+The `use()` function must be invoked before importing any other campy modules.
+Calling `use` after another `campy` module has been imported will have no
+effect. You should avoid explicitly calling `use()` unless absolutely necessary
+because your users will have to change your code if they want to use a
+different backend.
+
+Note: Backend name specifications are not case-sensitive, e.g. 'Tk' and 'tk'
+are equivalent.
+"""
+# TODO(sredmond): Add support for `use`.
+# TODO(sredmond): Add support for a .campyrc file.
+from campy.private.backends.tk.backend_tk import TkBackend
+from campy.private.backends.jbe.backend_jbe import JavaBackend
+
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+# The default backend to use when an environmental variable doesn't override.
+DEFAULT_BACKEND = 'Tk'
+
+backend_name = os.environ.get('CAMPY_BACKEND', DEFAULT_BACKEND).lower()
+logger.debug('Attempting to create backend {!r}'.format(backend_name))
+
+# Load a backend.
+# TODO(sredmond): This is effectively a singleton because Python only imports
+# modules once, unless forced to otherwise, such as with Jupyter's %autoreload
+# magic. To defend against forced module reimport, encapsulate this into
+# a proper singleton.
+if backend_name == 'tk':  # Load the Tk backend
+    backend = TkBackend()
+elif backend_name == 'jbe':  # Load the JBE backend.
+    backend = JavaBackend()
+else:
+    raise ImportError('Unrecognized backend: {!r}'.format(backend_name))
--- campy/private/backends/backend_base.py
+++ campy/private/backends/backend_base.py
@@ -1,107 +1,145 @@
-"""Common base class for all backends.
+"""Common abstract base classes for various backends.
 
-A backend provides several utilities
+There are three different types of backends.
 
+- GraphicsBackendBase: Everything graphical - windows, canvas objects, labels, interactors.
+- AudioBackendBase: Everything auditory - creating and playing sounds.
+- ConsoleBackendBase: Everything textual - fonts and lines.
+
+The Tk backend is a graphical backend only. The Java backend is all three.
+
+Other implementations of `ConsoleBackendBase` have been spun out into their own
+library `popup-console`.
+
+These abstract base classes are provided for backend developers to see which
+methods their backend will need to support. It is considered under development
+and can change at any time.
 """
+import abc
 
-class GraphicsBackendBase:
-    # GWindow Lifecycle
-    def gwindow_constructor(self, gw, width, height, top_compound, visible=True): pass
-    def gwindow_close(self, gw): pass
-    def gwindow_delete(self, gw): pass
-    def gwindow_set_exit_on_close(self, gw, exit_on_close): pass
+class GraphicsBackendBase(abc.ABC):
+    """Interface for graphical backends."""
+    # TODO(sredmond): Decorate all of these classes with @abc.abstractmethod.
+    # For now, the interlaced decorators make the interface too hard to read.
+
+    # GWindow lifecycle.
+    def gwindow_constructor(self, gwindow, width, height, top_compound, visible=True): pass
+    def gwindow_close(self, gwindow): pass
+    def gwindow_delete(self, gwindow): pass
+    def gwindow_set_exit_on_close(self, gwindow, exit_on_close): pass
     def gwindow_exit_graphics(self): pass
 
-    def gwindow_clear(self, gw): pass
-    def gwindow_clear_canvas(self, gw, exit_on_close): pass
-    def gwindow_add_to_region(self, gw, gobj, region): pass
-    def gwindow_request_focus(self, gw): pass
-    def gwindow_repaint(self, gw): pass
-    def gwindow_set_visible(self, flag, gobj = None, gw = None): pass
-    def gwindow_set_window_title(self, gw, title): pass
+    # GWindow drawing.
+    def gwindow_clear(self, gwindow): pass
+    def gwindow_clear_canvas(self, gwindow, exit_on_close): pass
+    def gwindow_repaint(self, gwindow): pass
+    def gwindow_draw(self, gwindow, gobject): pass
+
+    # GWindow attributes.
+    def gwindow_request_focus(self, gwindow): pass
+    def gwindow_set_visible(self, flag, gobject=None, gwindow=None): pass
+    def gwindow_set_window_title(self, gwindow, title): pass
     def gwindow_get_screen_width(self): pass
     def gwindow_get_screen_height(self): pass
-    def gwindow_draw(self, gw, gobj): pass
-    def gwindow_set_region_alignment(self, gw, region, align): pass
-    def gwindow_remove_from_region(self, gw, gobj, region): pass
 
-    def gobject_set_location(self, gobj, x, y): pass
-    def gobject_set_filled(self, gobj, flag): pass
-    def gobject_remove(self, gobj): pass
-    def gobject_set_color(self, gobj, color): pass
-    def gobject_set_fill_color(self, gobj, color): pass
-    def gobject_send_forward(self, gobj): pass
-    def gobject_send_to_front(self, gobj): pass
-    def gobject_send_backward(self, gobj): pass
-    def gobject_send_to_back(self, gobj): pass
-    def gobject_set_size(self, gobj, width, height): pass
-    def gobject_get_bounds(self, gobj): pass
-    def gobject_set_line_width(self, gobj, line_width): pass
-    def gobject_contains(self, gobj, x, y): pass
-    def gobject_scale(self, gobj, sx, sy): pass
-    def gobject_rotate(self, gobj, theta): pass
+    # GWindow geometry.
+    def gwindow_add_to_region(self, gwindow, gobject, region): pass
+    def gwindow_set_region_alignment(self, gwindow, region, align): pass
+    def gwindow_remove_from_region(self, gwindow, gobject, region): pass
+
+    # Shared GObject operations.
+    def gobject_set_location(self, gobject, x, y): pass
+    def gobject_set_filled(self, gobject, flag): pass
+    def gobject_remove(self, gobject): pass
+    def gobject_set_color(self, gobject, color): pass
+    def gobject_set_fill_color(self, gobject, color): pass
+    def gobject_send_forward(self, gobject): pass
+    def gobject_send_to_front(self, gobject): pass
+    def gobject_send_backward(self, gobject): pass
+    def gobject_send_to_back(self, gobject): pass
+    def gobject_set_size(self, gobject, width, height): pass
+    def gobject_get_size(self, gobject): pass
+    def gobject_get_bounds(self, gobject): pass
+    def gobject_set_line_width(self, gobject, line_width): pass
+    def gobject_contains(self, gobject, x, y): pass
+    def gobject_scale(self, gobject, sx, sy): pass
+    def gobject_rotate(self, gobject, theta): pass
 
     # Rectangular regions
-    def grect_constructor(self, gobj, width, height): pass
-    def groundrect_constructor(self, gobj, width, height, corner): pass
-    def g3drect_constructor(self, gobj, width, height, raised): pass
-    def g3drect_set_raised(self, gobj, raised): pass
+    def grect_constructor(self, gobject, width, height): pass
+    def groundrect_constructor(self, gobject, width, height, corner): pass
+    def g3drect_constructor(self, gobject, width, height, raised): pass
+    def g3drect_set_raised(self, gobject, raised): pass
 
     # Elliptical regions
-    def goval_constructor(self, gobj, width, height): pass
-    def garc_constructor(self, gobj, width, height, start, sweep): pass
-    def garc_set_start_angle(self, gobj, angle): pass
-    def garc_set_sweep_angle(self, gobj, angle): pass
-    def garc_set_frame_rectangle(self, gobj, x, y, width, height): pass
+    def goval_constructor(self, gobject, width, height): pass
+    def garc_constructor(self, gobject, width, height, start, sweep): pass
+    def garc_set_start_angle(self, gobject, angle): pass
+    def garc_set_sweep_angle(self, gobject, angle): pass
+    def garc_set_frame_rectangle(self, gobject, x, y, width, height): pass
 
     # GLines
-    def gline_constructor(self, gobj, x1, y1, x2, y2): pass
-    def gline_set_start_point(self, gobj, x, y): pass
-    def gline_set_end_point(self, gobj, x, y): pass
+    def gline_constructor(self, gobject, x1, y1, x2, y2): pass
+    def gline_set_start_point(self, gobject, x, y): pass
+    def gline_set_end_point(self, gobject, x, y): pass
 
     # Compounds
-    def gcompound_constructor(self, gobj): pass
-    def gcompound_add(self, compound, gobj): pass
+    def gcompound_constructor(self, gobject): pass
+    def gcompound_add(self, compound, gobject): pass
 
-    def gimage_constructor(self, gobj,  filename): pass
-    def glabel_constructor(self, gobj, label): pass
-    def glabel_set_font(self, gobj, font): pass
-    def glabel_set_label(self, gobj, str): pass
-    def glabel_get_font_ascent(self, gobj): pass
-    def glabel_get_font_descent(self, gobj): pass
-    def glabel_get_size(self, gobj): pass
-    def gpolygon_constructor(self, gobj): pass
-    def gpolygon_add_vertex(self, gobj, x, y): pass
+    # Labels
+    def glabel_constructor(self, gobject, label): pass
+    def glabel_set_font(self, gobject, font): pass
+    def glabel_set_label(self, gobject, str): pass
+    def glabel_get_font_ascent(self, gobject): pass
+    def glabel_get_font_descent(self, gobject): pass
+    def glabel_get_size(self, gobject): pass
+
+    # Polygons
+    def gpolygon_constructor(self, gobject): pass
+    def gpolygon_add_vertex(self, gobject, x, y): pass
+
+    # Timers
     def gtimer_constructor(self, timer, millis): pass
     def gtimer_delete(self, timer): pass
     def gtimer_start(self, timer): pass
     def gtimer_pause(self, millis): pass
     def gtimer_stop(self, timer): pass
-    def gbufferedimage_constructor(self, gobj, x, y, width, height): pass
-    def gbufferedimage_fill(self, gobj, rgb): pass
-    def gbufferedimage_fill_region(self, gobj, x, y, width, height, rgb): pass
-    def gbufferedimage_load(self, gobj, filename): pass
-    def gbufferedimage_resize(self, gobj, width, height, retain): pass
-    def gbufferedimage_save(self, gobj, filename): pass
-    def gbufferedimage_set_rgb(self, gobj, x, y, rgb): pass
-    def gbufferedimage_update_all_pixels(self, gobj, base64): pass
 
-    def setActionCommand(self, gobj, cmd): pass
-    def getSize(self, gobj): pass
-    def gbutton_constructor(self, gobj, label): pass
-    def gcheckbox_constructor(self, gobj, label): pass
-    def gcheckbox_is_selected(self, gobj): pass
-    def gcheckbox_set_selected(self, gobj, state): pass
-    def gslider_constructor(self, gobj, min, max, value): pass
-    def gslider_get_value(self, gobj): pass
-    def gslider_set_value(self, gobj, value): pass
-    def createGTextField(self, gobj, num_chars): pass
-    def getText(self, gobj): pass
-    def setText(self, gobj, str): pass
-    def createGChooser(self, gobj): pass
-    def addItem(self, gobj, item): pass
-    def getSelectedItem(self, gobj): pass
-    def setSelectedItem(self, gobj, item): pass
+    # Images
+    def gimage_constructor(self, gobject,  filename): pass
+
+    def gbufferedimage_constructor(self, gobject, x, y, width, height): pass
+    def gbufferedimage_fill(self, gobject, rgb): pass
+    def gbufferedimage_fill_region(self, gobject, x, y, width, height, rgb): pass
+    def gbufferedimage_load(self, gobject, filename): pass
+    def gbufferedimage_resize(self, gobject, width, height, retain): pass
+    def gbufferedimage_save(self, gobject, filename): pass
+    def gbufferedimage_set_rgb(self, gobject, x, y, rgb): pass
+    def gbufferedimage_update_all_pixels(self, gobject, base64): pass
+
+    # Events
+    def set_action_command(self, gobject, cmd): pass
+    def get_next_event(self, mask): pass
+    def wait_for_event(self, mask): pass
+
+    # Interactors
+    def gbutton_constructor(self, gobject, label): pass
+    def gcheckbox_constructor(self, gobject, label): pass
+    def gcheckbox_is_selected(self, gobject): pass
+    def gcheckbox_set_selected(self, gobject, state): pass
+    def gslider_constructor(self, gobject, min, max, value): pass
+    def gslider_get_value(self, gobject): pass
+    def gslider_set_value(self, gobject, value): pass
+    def gtextfield_constructor(self, gobject, num_chars): pass
+    def gtextfield_get_text(self, gobject): pass
+    def gtextfield_set_text(self, gobject, str): pass
+    def gchooser_constructor(self, gobject): pass
+    def gchooser_add_item(self, gobject, item): pass
+    def gchooser_get_selected_item(self, gobject): pass
+    def gchooser_set_selected_item(self, gobject, item): pass
+
+    # Dialogs
     def file_open_file_dialog(self, title, mode, path): pass
     def gfilechooser_show_open_dialog(self, current_dir, file_filter): pass
     def gfilechooser_show_save_dialog(self, current_dir, file_filter): pass
@@ -111,51 +149,37 @@ class GraphicsBackendBase:
     def goptionpane_show_option_dialog(self, message, title, options, initially_selected): pass
     def goptionpane_show_text_file_dialog(self, message, title, rows, cols): pass
 
-    def getNextEvent(self, mask): pass
-    def waitForEvent(self, mask): pass
 
-    def parseEvent(self, line): pass
-    def parseMouseEvent(self, tokens, type): pass
-    def parseKeyEvent(self, tokens, type): pass
-    def parseTimerEvent(self, tokens, type): pass
-    def parseWindowEvent(self, tokens, type): pass
-    def parseActionEvent(self, tokens, type): pass
+class ConsoleBackendBase(abc.ABC):
+    """Interface for console backends."""
 
-    def startupMain(self): pass
+    @abc.abstractmethod
+    def clear_console(self): pass
+    @abc.abstractmethod
+    def set_console_font(self, font): pass
+    @abc.abstractmethod
+    def set_console_size(self, console_size): pass
+    @abc.abstractmethod
+    def get_console_line(self): pass
+    @abc.abstractmethod
+    def put_console(self, line, stderr=False): pass
+    @abc.abstractmethod
+    def echo_console(self): pass
+    @abc.abstractmethod
+    def end_line_console(self): pass
 
-class ConsoleBackendBase:
-    def clear_console(self):
-        pass
 
-    def set_console_font(self, font):
-        pass
+class AudioBackendBase(abc.ABC):
+    """Interface for audio backends."""
 
-    def set_console_size(self, console_size):
-        pass
+    # Sounds
+    @abc.abstractmethod
+    def create_sound(self, sound, *args): pass
+    @abc.abstractmethod
+    def delete_sound(self, sound, *args): pass
+    @abc.abstractmethod
+    def play_sound(self, sound, *args): pass
 
-    def get_console_line(self):
-        pass
-
-    def put_console(self, line, stderr=False):
-        pass
-
-    def echo_console(self):
-        pass
-
-    def end_line_console(self):
-        pass
-
-class SoundBackendBase:
-    """A `SoundBackendBase` provides a common API for loading and playing sounds."""
-
-    def create_sound(self, sound, *args):
-        pass
-
-    def delete_sound(self, sound, *args):
-        pass
-
-    def play_sound(self, sound, *args):
-        pass
-
-    def note_play(self, note, repeat):
-        pass
+    # Notes
+    @abc.abstractmethod
+    def play_note(self, note, repeat): pass
