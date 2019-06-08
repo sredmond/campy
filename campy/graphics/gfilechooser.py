a1c2620d7d8fb3f7c65ea9fb3873566bca6a3395
Sam Redmond <sredmond@stanford.edu>
2019-06-08 01:54:33 -0700

    Adds extensive documentation to GPoint, GDimension, and GRectangle and cleans GFileChooser.

--- campy/graphics/gfilechooser.py
+++ campy/graphics/gfilechooser.py
@@ -1,23 +1,39 @@
-"""A :class:`GFileChooser` pops up a graphical dialog boxes to select a file.
+"""Show graphical dialog boxes that ask the user to select a file.
 
 These dialog boxes come in two flavors:
 
-- Open Dialog: Select a file to open.
-- Save Dialog: Select a file to save.
+- Open: Select a file to open.
+- Save: Select a file to save.
+
+In each case, the filename is returned, or None if the user closed the dialog.
 """
+# TODO(sredmond): Reconsider having the empty string returned as a sentinel.
+# TODO(sredmond): Add support for additional parameters to the dialogs.
+# e.g. defaultextension, title)
 import campy.private.platform as _platform
 
-# TODO(sredmond): If this class only has static methods, then they should just
-# be module-level functions.
-class GFileChooser:
-    # Constants for dialog types, taken from Java's JFileChooser
-    SAVE_DIALOG = 1
-    OPEN_DIALOG = 1
+import os
 
-    @staticmethod
-    def show_open_dialog(current_dir="", file_filter=""):
-        return _platform.Platform().gfilechooser_show_open_dialog(current_dir, file_filter)
 
-    @staticmethod
-    def show_save_dialog(current_dir="", file_filter=""):
-        return _platform.Platform().gfilechooser_show_save_dialog(current_dir, file_filter)
+# The current user's home directory, used as the default initial directory.
+HOME = os.path.expanduser('~')
+
+
+def show_open_dialog(directory=HOME):
+    """Show the user a dialog asking for a file to open.
+
+    :param directory: The initial directory displayed in the dialog.
+    :type directory: pathlib.Path or str
+    :return: The filename of the user-selected file, or None if the dialog was cancelled.
+    """
+    return _platform.Platform().gfilechooser_show_open_dialog(str(directory), file_filter='')
+
+
+def show_save_dialog(directory=HOME):
+    """Show the user a dialog asking for a file to save.
+
+    :param directory: The initial directory displayed in the dialog.
+    :type directory: pathlib.Path or str
+    :return: The filename of the user-selected file, or None if the dialog was cancelled.
+    """
+    return _platform.Platform().gfilechooser_show_save_dialog(str(directory), file_filter='')
--- campy/graphics/gtypes.py
+++ campy/graphics/gtypes.py
@@ -1,90 +1,138 @@
+"""Exported types for representing points, dimensions, and rectangles.
+
+A :class:`GPoint` represents an (x, y) pair and defaults to (0, 0).
+
+A :class:`GDimension` represents a (width, height) pair and defaults to (0, 0).
+
+A :class:`GRectangle` represents an (x, y, width, height) quadruplet and
+defaults to (0, 0, 0, 0).
+
+Named attributes are accessible for each of these types::
+
+    point = GPoint(4, 1)
+    print(point.x, point.y)  # => 4, 1
+
+    size = GDimension(41, 574)
+    print(size.width, size.height)  # => 41, 574
+
+    rect = GRectangle(4, 1, 41, 574)
+    print(rect.x, rect.y, rect.width, rect.height)  # => 4, 1, 41, 574
+
+Each of these types can also be unpacked like a tuple.
+
+    point = GPoint(4, 1)
+    x, y = point
+
+    size = GDimension(41, 574)
+    width, height = size
+
+    rect = GRectangle(4, 1, 41, 574)
+    x, y, width, height = rect
+
+Lastly, each of these types is immutable.
 """
-This module defines classes for representing points, dimensions, and rectangles.
+# NOTE: There's a lot of hacking around with the internals of namedtuple in
+# this module. This level of hacking is not encouraged, and might be removed
+# in the future, replacing these types with custom-style namedtuples.
+# Here be dragons.
+# TODO(sredmond): Redocument these types so that Sphinx's autodoc sees them.
+# TODO(sredmond): Reconsider defaulting all arguments to 0.0 instead of 0.
+import collections
+import operator
+
+
+# An (x, y) pair defaulting to (0, 0).
+GPoint = collections.namedtuple('GPoint', ('x', 'y'))
+GPoint.__new__.__defaults__ = (0, 0)
+
+
+# A (width, height) pair defaulting to (0, 0).
+GDimension = collections.namedtuple('GDimension', ('width', 'height'))
+GDimension.__new__.__defaults__ = (0, 0)
+GDimension.empty = lambda dim: dim.width <= 0 or dim.height <= 0
+GDimension.empty.__name__ = 'empty'
+
+
+# An (x, y, width, height) quadruplet defaulting to (0, 0, 0, 0)
+GRectangle = collections.namedtuple('GRectangle', ('x', 'y', 'width', 'height'))
+GRectangle.__new__.__defaults__ = (0, 0, 0, 0)
+GRectangle.empty = lambda rect: rect.width <= 0 or rect.height <= 0
+GRectangle.empty.__name__ = 'empty'
+# NOTE(sredmond): Since we consider the edge to be contained, "empty" GRectangles can still contain points.
+GRectangle.__contains__ = lambda rect, point: 0 <= point[0] - rect.x <= rect.width and 0 <= point[1] - rect.y <= rect.height
+GRectangle.__contains__.__name__ = '__contains__'
+
+
+# Override docstrings. This is perhaps the messiest part of this file.
+GPoint.__doc__ = 'Graphical representation of an (x, y) point defaulting to (0, 0).'
+GPoint.__new__.__doc__ = """Create a :class:`GPoint`.
+
+If parameters are not supplied, all arguments default to 0.
+
+All arguments have units of pixels.
+
+:param x: The x-coordinate (in pixels).
+:param y: The y-coordinate (in pixels).
 """
-from collections import namedtuple as _nt
+GPoint.x = property(operator.itemgetter(0), doc='The x-coordinate (in pixels).')
+GPoint.y = property(operator.itemgetter(1), doc='The y-coordinate (in pixels).')
 
-GPoint = _nt('GPoint', ['x', 'y'])
-GPoint.__new__.__defaults__ = (0.0, 0.0)
 
-GDimension = _nt('GDimension', ['width', 'height'])
-GDimension.__new__.__defaults__ = (0.0, 0.0)
+GDimension.__doc__ = 'Graphical representation of a size (width, height) defaulting to (0, 0).'
+GDimension.__new__.__doc__ = """Create a :class:`GDimension`.
 
-class GRectangle(object):
-    '''
-    This type contains real-valued x, y, width, and height fields. It is used to
-    represent the bounding box of a graphical object
-    '''
-    def __init__(self, x=0.0, y=0.0, width=0.0, height=0.0):
-        '''
-        Initializes a GRectangle object with the specified fields. If these
-        parameters are not supplied hte default values are set to 0.0
+If parameters are not supplied, all arguments default to 0.
 
-        @type x: float
-        @param x: x coordinate of upper left corner
-        @type y: float
-        @param y: y coordinate of upper left corner
-        @type width: float
-        @param width: width of rectangle
-        @type height: float
-        @param height: height of rectangle
-        @rtype: void
-        '''
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+All arguments have units of pixels.
 
-    def is_empty(self):
-        '''
-        Returns if the rectangle is empty.
+:param width: The width (in pixels).
+:param height: The height (in pixels).
+"""
+GDimension.width = property(operator.itemgetter(0), doc='The width (in pixels).')
+GDimension.height = property(operator.itemgetter(1), doc='The height (in pixels).')
+GDimension.empty.__doc__ = 'Return whether this rectangle is empty.'
 
-        @rtype: boolean
-        '''
-        return self.width <= 0 or self.height <= 0
 
-    def __contains__(self, point):
-        '''
-        Returns true if the rectangle contains the given point, which may be
-        specified either as a GPoint or distinct coordinates
+GRectangle.__doc__ = """Graphical rectangle with upper-left corner (x, y) and size (width, height).
 
-        @type pt: GPoint
-        @param pt: GPoint to check, will override x and y
-        @type x: float
-        @param x: x coordinate of point to check
-        @type y: float
-        @param y: y coordinate of point to check
-        @rtype: boolean
-        '''
-        x, y = point
-        return self.x <= x <= self.x + self.width and \
-               self.y <= y <= self.y + self.height
+A :class:`GRectangle` is often used to represent the bounding box of a graphical object.
+"""
+GRectangle.__new__.__doc__ = """Create a :class:`GRectangle`.
 
-if __name__ == '__main__':
-    print("Testing gtypes.py")
-    print("--------------------------------------------------")
+The x- and y-coordinates refer to the upper-left corner of this
+:class:`GRectangle`.
 
-    print("Create GPoint default values")
-    gp1 = GPoint()
-    x = gp1.x
-    y = gp1.y
-    tostring = str(gp1)
-    gp2 = GPoint()
-    eq = (gp1 == gp2)
-    ne = (gp1 != gp2)
-    if(x != 0.0 or y != 0.0 or tostring != "GPoint(x=0.0, y=0.0)" or eq != True or ne != False):
-        print("FAILED")
-    print("PASSED")
+If parameters are not supplied, all arguments default to 0.
 
-    print("Create GPoint non-default values")
-    gp1 = GPoint(5, -2.5)
-    x = gp1.x
-    y = gp1.y
-    tostring = str(gp1)
-    gp2 = GPoint(5, -2.5)
-    eq = (gp1 == gp2)
-    ne = (gp1 != gp2)
-    if(x != 5.0 or y != -2.5 or tostring != "GPoint(x=5, y=-2.5)" or eq != True or ne != False):
-        print("FAILED")
-    print("PASSED")
-    print
+All arguments have units of pixels.
 
+:param x: The x-coordinate of the upper left corner (in pixels).
+:param y: The y-coordinate of the upper left corner (in pixels).
+:param width: The width of this rectangle (in pixels).
+:param height: The height of this rectangle (in pixels).
+"""
+GRectangle.x = property(operator.itemgetter(0), doc='The x-coordinate of the upper left corner (in pixels).')
+GRectangle.y = property(operator.itemgetter(1), doc='The y-coordinate of the upper left corner (in pixels).')
+GRectangle.width = property(operator.itemgetter(2), doc='The width of this rectangle (in pixels).')
+GRectangle.height = property(operator.itemgetter(3), doc='The height of this rectangle (in pixels).')
+GRectangle.empty.__doc__ = 'Return whether this rectangle is empty.'
+GRectangle.__contains__.__doc__ = """Return whether this rectangle contains a point.
+
+The edges of the rectangle are considered to be contained by the rectangle.
+For example, the point (4, 1) is contained in the rectangle (4, 1, 41, 574).
+
+The point must be unpackable into two elements, which means that you can use a
+:class:`GPoint` or a two-element tuple::
+
+    rect = GRectangle(4, 1, 41, 574)
+    point = GPoint(4, 1)
+    if point in rect:  # Using a GPoint.
+        print('GPoint is contained.')
+    if (5, 3) in rect:  # Using a tuple.
+        print('tuple is contained')
+
+:param point: An (x, y) point to check for containment.
+:return: Whether this rectangle contains the given point.
+"""
+
+__all__ = ('GPoint', 'GDimension', 'GRectangle')
