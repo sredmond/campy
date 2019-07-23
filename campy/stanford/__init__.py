c4a329ffe35c7bc3cb9acebce5e6ac8393e6b277
Sam Redmond <sredmond@stanford.edu>
2019-07-23 02:43:51 -0700

    Only writes to TTY outputs.

--- campy/stanford/__init__.py
+++ campy/stanford/__init__.py
@@ -1,4 +1,11 @@
-"""Get it?! This is the `campy.stanford` subpackage. Like Camp Stanford!"""
-print('Did you mean camp.stanford?')
-print('Like "Camp Stanford!"')
-print("I'm funny!")
+"""Welcome to `campy.stanford`.
+
+See:
+    https://www.stanforddaily.com/2011/04/26/one-time-at-camp-stanford/
+"""
+import sys
+if sys.stdout.isatty():
+    print('Importing `campy.stanford`...')
+    print('Import failed.')
+    print('Importing `camp.stanford` instead...')
+    print('☀️🌲🏕️🌲☀️')
