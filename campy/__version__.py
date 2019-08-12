a082985375abf382341d226fc52300818f9dffe6
Sam Redmond <sredmond@stanford.edu>
2019-08-12 16:26:42 -0700

    Prepares for dev19.

--- campy/__version__.py
+++ campy/__version__.py
@@ -3,7 +3,7 @@ __description__ = "Stanford's Graphical Libraries in Python"
 __url__ = 'https://campy.sredmond.io/'
 __license__ = 'MIT'
 
-__version__ = '0.0.1.dev18'
+__version__ = '0.0.1.dev19'
 __build__ = 0x000001
 __status__ = 'Alpha'
 
